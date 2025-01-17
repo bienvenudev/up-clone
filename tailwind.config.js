9e83f3b65b3b14c1d39a133c184a3124e4c0ab74
bienvenudev <cbienvenu007@gmail.com>
2025-01-17 20:30:15 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "primary-100": "#d5d5d5",
+        "primary-200": "#212529",
         "primary-300": "#333",
+        "primary-400": "#f2f2f2",
         "secondary-100": "#a18a4d",
       },
     },
